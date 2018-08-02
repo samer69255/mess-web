60e02315c97bc127a5920fee46e1b9fe1efad0ba
zaincashu <zaincashu@gmail.com>
2018-08-02 19:33:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,240 @@
+//This is still work in progress
+/*
+Please report any bugs to nicomwaks@gmail.com
+i have added console.log on line 48
+ */
+
+
+
+
+
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
+const app = express();
+app.use(express.static('public'));
+var gis = require('g-i-s');
+const fb = require('./fb');
+
+
+
+app.set('port', (process.env.PORT || 5000));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+var IDS = {}
+
+// index
+app.get('/', function (req, res) {
+   res.end('');
+});
+
+
+
+// for facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'samer') {
+        res.send(req.query['hub.challenge']);
+        console.log(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong token');
+    }
+})
+
+// to post data
+app.post('/webhook/', function (req, res) {
+
+   // console.log(cmds);
+    var messaging_events = req.body.entry[0].messaging;
+    console.log(messaging_events);
+    res.sendStatus(200);
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+
+
+
+        if (event.message && event.message.text) {
+
+            var re;
+
+            var text = event.message.text;
+            console.log(text);
+            text = text.trim().toLowerCase();
+            
+            if (sender in IDS) {
+                  if (text == '-stop') {
+                if (IDS[sender].s == 'p') IDS[sender].plist = [];
+                else if (IDS[sender].s == 'v') IDS[sender].vlist = [];
+            }
+            
+            else if (text == '-new') {
+                delete IDS[sender];
+            }
+                else {
+                    
+                        var cmd = IDS[sender];
+                if (cmd == 'p') photos(text,sender);
+                else if (cmd == 'v') video(text,sender);
+                else if (cmd == 'f') sendfile(text,sender);
+                else {
+                    delete IDS[sender];
+                    sendTextMessage(sender,'حديث خطأ الرجاء البدء من جديد');
+                }
+                    
+                }
+            
+                return;
+            }
+           
+            
+            if (text.indexOf('photo') || text.indexOf('صور'))
+                {
+                    
+                    IDS[sender] = {s:'p'};
+                    sendTextMessage(sender,'اكتب عبارة البحث عن الصور');
+                }
+            else if (text.indexOf('video') || text.indexOf('youtube') || cmd.indexOf('فيديو') || cmd.indexOf('يوتيوب')) {
+                IDS[sender] ={s:'v'};
+                sendTextMessage(sender,'ماذا تريد من مفاطع الفيديو؟');
+            }
+            
+            else if (text.indexOf('file') || text.indexOf('ملف')) {
+                IDS[sender] = {s:'f'};
+                sendPhotoMessage(sender,'اكتب لي رابط الملف');
+            }
+            
+           
+            
+            
+            
+            else {
+                sendTextMessage(sender,getHelp());
+            }
+
+
+
+
+
+
+
+        }
+        if (event.postback) {
+            var text = JSON.stringify(event.postback);
+            fb.sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+            continue;
+        }
+    }
+    
+});
+
+// recommended to inject access tokens as environmental variables, e.g.
+// const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+function photos(text,sender) {
+    
+    if (!text) return fb.sendTextMessage(sender,'لم اتعرف على النص,الرجاء كتابة عبارة صالحة')
+    
+    var re = 'انا الآن ابحث لك';
+
+
+
+              fb.sendTextMessage(sender,re);
+            
+            search(text,function(resu) {
+                re = 'وجدت لك'+resu.length+' صورة';
+                fb.sendTextMessage(sender,re);
+                IDS[sender].plist=resu;
+                startSendPotos(sender);
+            });
+
+}
+
+
+function video(text,sender) {
+    if (!text) return fb.sendTextMessage(sender,'use:\nv:[video]');
+    fb.sendTextMessage(sender,'working for '+text);
+    srh_video(sender,text);
+}
+
+function sendfile(text,sender) {
+    if (!text) return fb.sendTextMessage(sender,'use:\nu:[url]');
+    fb.sendTextMessage(sender,'working for '+text);
+}
+
+
+
+
+function search(text,fn) {
+    
+    
+    var list = [];
+    gis(text, logResults);
+
+function logResults(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    for (var i in results)
+        {
+            list.push(results[i].url);
+        }
+      
+      fn(list);
+  }
+}
+    
+    
+    
+}
+
+
+function srh_video(sender,text) {
+     var search_v = require('youtube-search');
+
+var opts = {
+  maxResults: 2,
+  key: 'AIzaSyAPyZWOyC70TvVqJWAQVzsa6t1-b8T8gkY'
+};
+
+search_v('اختراق حسابات فيس ', opts, function(err, results) {
+  if(err) return console.log(err);
+    fb.sendVideo(sender,results[0]);
+});
+}
+
+
+function startSendPotos(sender) {
+    var f = function () {
+        
+        if (IDS[sender].list.length >= 1)
+            {
+                IDS[sender].list.push();
+                sendFileMessage(sender,{type:'image',url:IDS[sender][0]},f);
+            }
+            
+        else {
+            sendTextMessage(sender,'ok');
+        }
+    }
+    
+    sendFileMessage(sender,{type:'image',url:IDS[sender][0]},f);
+}
+
+
+
+// spin spin sugar
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+});
---
+++ fb.js
@@ -0,0 +1,117 @@
+
+const request = require('request');
+var s;
+var token = "EAAELRvdKfxEBAP3cXdhHbh01sUalCZCGqZBKRL6ZCdPZAE3UjZC95A9LVVmCFhQTaFHALIL87RGOBgKF7SFB6Ti4gjd8fZA2t2QeUlmIIQOS9D2XFmC5aZBM3hxDNRX0WguyW7vfdylnczsWNTxpmZCxpwBos7DnZBpACuokckNT10HyNpkUvq7DM";
+
+function sendTextMessage(sender, text) {
+    
+    var messageData = { text:text };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            {console.log('Error sending messages: ', error);
+               
+            }
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    });
+}
+
+
+
+
+function sendFileMessage(sender,ob,fn) {
+    var messageData = { attachment:{
+        "type":ob.type,
+        "payload":{
+        "url":ob.url, 
+        "is_reusable":true
+      }
+    } };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            {console.log('Error sending messages: ', error);
+               
+            }
+        }
+        else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+            
+        }
+        
+        fn();
+        
+    });
+}
+
+
+
+function sendVideo(sender,v) {
+    
+       var token = "EAAELRvdKfxEBAP3cXdhHbh01sUalCZCGqZBKRL6ZCdPZAE3UjZC95A9LVVmCFhQTaFHALIL87RGOBgKF7SFB6Ti4gjd8fZA2t2QeUlmIIQOS9D2XFmC5aZBM3hxDNRX0WguyW7vfdylnczsWNTxpmZCxpwBos7DnZBpACuokckNT10HyNpkUvq7DM";
+    var messageData = { attachment:{
+        "type":"video",
+        "payload":{
+        "url":v.link, 
+        "is_reusable":true
+      }
+    } };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            {console.log('Error sending messages: ', error);
+               
+            }
+        }
+        else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+            
+        }
+        
+        sendTextMessage(sender,v.link);
+        
+    });
+
+    
+}
+
+function sendfile() {
+    
+}
+
+
+function stop() {
+    s = false;
+}
+
+
+exports.sendTextMessage = sendTextMessage;
+exports.sendPhotoMessage = sendPhotoMessage;
+exports.stop = stop;
+exports.sendVideo = sendVideo;
